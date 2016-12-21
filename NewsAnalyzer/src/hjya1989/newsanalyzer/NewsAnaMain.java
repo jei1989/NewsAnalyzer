deac4f01818fc90ad9d89a822fe313a9d486c1f8
Administrator <Administrator@shj_pc>
2016-12-22 01:39:50 +0900

    2016.12.22

---
+++ NewsAnalyzer/src/hjya1989/newsanalyzer/NewsAnaMain.java
@@ -0,0 +1,21 @@
+package hjya1989.newsanalyzer;
+
+public class NewsAnaMain {
+
+	public NewsAnaMain()
+	{
+		
+	}
+	
+	public void init(){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new NewsAnaMain();
+		
+	}
+
+}
